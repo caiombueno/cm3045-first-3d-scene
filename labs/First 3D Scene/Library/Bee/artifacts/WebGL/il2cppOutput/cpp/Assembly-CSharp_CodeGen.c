c0ab0e97a9bf9f09936dae4a9c38bbe9fa109666
caiombueno <caiomeirabueno@hotmail.com>
2024-06-21 15:03:48 -0300

    initial commit

---
+++ labs/First 3D Scene/Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,65 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void CameraPosition_Start_m742A56DE6B103FDC0C0213B0CDECC32DA2AEACF2 (void);
+extern void CameraPosition_Update_m0BDB379F8A3CD193A12D8825C77231FE1A88C89F (void);
+extern void CameraPosition__ctor_m134CE0B406131D93BE174830690AC6493018E43B (void);
+extern void DaveMovement_Start_m2F15DE61F96F1FDFD1C16BDCB7630BE37B53286D (void);
+extern void DaveMovement_Update_mA2D520EF07D58B70131398AB20B16C5A1B02B9F6 (void);
+extern void DaveMovement__ctor_m52C50F05061644925C1A67CF14096E57DE7A4D4E (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[8] = 
+{
+	CameraPosition_Start_m742A56DE6B103FDC0C0213B0CDECC32DA2AEACF2,
+	CameraPosition_Update_m0BDB379F8A3CD193A12D8825C77231FE1A88C89F,
+	CameraPosition__ctor_m134CE0B406131D93BE174830690AC6493018E43B,
+	DaveMovement_Start_m2F15DE61F96F1FDFD1C16BDCB7630BE37B53286D,
+	DaveMovement_Update_mA2D520EF07D58B70131398AB20B16C5A1B02B9F6,
+	DaveMovement__ctor_m52C50F05061644925C1A67CF14096E57DE7A4D4E,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[8] = 
+{
+	1113,
+	1113,
+	1113,
+	1113,
+	1113,
+	1113,
+	2155,
+	1113,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	8,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
